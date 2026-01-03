91eda1a9fdfda6c9e153ea10eef4a51601c3b3e1
akcabra <andraz.klobucar@gmail.com>
2026-01-03 21:02:45 +0100

    HIKU-21: Dodan keycloak

--- src/main/java/com/hiku/trailImport/TrailImportService.java
+++ src/main/java/com/hiku/trailImport/TrailImportService.java
@@ -2,7 +2,11 @@ package com.hiku.trailImport;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import org.eclipse.microprofile.auth.LoginConfig;
+import javax.annotation.security.DeclareRoles;
 
-@ApplicationPath("/trail-import")
+@LoginConfig(authMethod = "MP-JWT")
+@DeclareRoles({"user", "admin"})
+@ApplicationPath("/api/trail-import")
 public class TrailImportService extends Application {
 }
